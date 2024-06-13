9f6578cd48b52d59fd56b812fca02d60909938b0
bern4rd <felipebernard.pe@gmail.com>
2024-06-13 17:03:53 -0300

    fix: adjust script to run both devices on test

--- run_tests.ps1
+++ run_tests.ps1
@@ -1,18 +1,43 @@
-# robot --variable PLATFORM:Android your_file.robot
-
+# Defina o arquivo contendo os caminhos
 $arq_e2e = "resources/devdata/path_tests.txt"
-$platforms = @('Android', 'iOS')
 
+# Lista para armazenar os objetos de trabalho
+$jobs = @()
+
+# Leia o conteúdo do arquivo e processe cada linha
 Get-Content $arq_e2e | ForEach-Object {
+    $testPath = $_
+    $item = ($testPath -split '/' | Select-Object -Index 1) -split '\.' | Select-Object -Index 0
+    
+    # Defina o caminho de saída para cada plataforma
+    $outputPathAndroid = "logs/Android/$item/"
+    $outputPathiOS = "logs/iOS/$item/"
+    
+    # Execute os comandos em paralelo
+    $jobAndroid = Start-Job -ScriptBlock {
+        param ($testPath, $outputPathAndroid)
+        Write-Host "Running tests ($testPath) for Android"
+        robot --variable PLATFORM:Android --outputdir $outputPathAndroid $testPath
+    } -ArgumentList $testPath, $outputPathAndroid
 
-    ForEach ($platform in $platforms){
-        $path = $_
-        $item = ($path -split '/' | Select-Object -Index 1) -split '\.' | Select-Object -Index 0
+    $jobiOS = Start-Job -ScriptBlock {
+        param ($testPath, $outputPathiOS)
+        Write-Host "Running tests ($testPath) for iOS"
+        robot --variable PLATFORM:iOS --outputdir $outputPathiOS $testPath
+    } -ArgumentList $testPath, $outputPathiOS
 
-        $outputPath = "logs/${platform}/${item}/"
-
-        $command = "robot --variable PLATFORM:${platform} --outputdir ${outputPath} ${path}"
-        Write-Host "`n Executando o teste no <$platform> com o comando : $command `n"
-        Invoke-Expression $command
-    }
+    $jobs += $jobAndroid
+    $jobs += $jobiOS
 }
+
+# Aguardar a conclusão de todos os trabalhos
+$jobs | Wait-Job
+
+# Obter e exibir os resultados dos trabalhos
+$jobs | ForEach-Object {
+    $output = Receive-Job -Job $_
+    $output
+}
+
+# Remover todos os trabalhos
+$jobs | Remove-Job
